d32c28da54a55b748e9451322b87c695ae863d14
Cutozki <98546755+Cutozki@users.noreply.github.com>
2022-06-08 23:24:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -566,8 +566,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about_menu":
         buttons = [[
-        InlineKeyboardButton('ᴅᴋ ʙᴏᴛx', url='https://t.me/dk_botx'),
-        InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url='https://t.me/dkbotxchats'),
+        InlineKeyboardButton('CHANNEL', url='https://t.me/Mkvtvseris'),
+        InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url='https://t.me/Bot_Updates_X'),
         InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
